c78356c8528ed9b91f1db43c4d0d01d4e84025ce
skreider-ce <skreider@corevitas.com>
2024-02-15 13:33:38 -0500

    check for file

--- R/00_generateReport.R
+++ R/00_generateReport.R
@@ -11,7 +11,8 @@
 generateReport <- function(.inputDatasetUrl, .reportOutputUrl, .fileName){
   .checkDataset <- readRDS(glue::glue("{.inputDatasetUrl}"))
 
-  rmarkdown::render(system.file("rmd", "template.Rmd", package = "registrydqchecksreportdown")
+  file.exists(system.file("rmarkdown", "template.Rmd", package = "registrydqchecksreportdown"))
+  rmarkdown::render(system.file("rmarkdown", "template.Rmd", package = "registrydqchecksreportdown")
                     ,output_dir = .reportOutputUrl
                     ,output_file = glue::glue("{.fileName}.html")
                     ,params = list(checkDataset = .checkDataset))
