c60015a0d54d560cd27bfe92373ff1ccb1d12d29
skreider-ce <skreider@corevitas.com>
2024-02-15 14:00:20 -0500

    remove intermediate markdown file

--- R/00_generateReport.R
+++ R/00_generateReport.R
@@ -11,9 +11,10 @@
 generateReport <- function(.inputDatasetUrl, .reportOutputUrl, .fileName){
   .checkDataset <- readRDS(glue::glue("{.inputDatasetUrl}"))
 
-  file.exists(system.file("rmarkdown", "template.Rmd", package = "registrydqchecksreportdown"))
   rmarkdown::render(system.file("rmarkdown", "template.Rmd", package = "registrydqchecksreportdown")
                     ,output_dir = .reportOutputUrl
                     ,output_file = glue::glue("{.fileName}.html")
+                    ,output_format = "html_document"
                     ,params = list(checkDataset = .checkDataset))
+  file.remove(glue::glue("{.reportOutputUrl}{.fileName}.md"))
 }
