deb799d3e8ccaebb76274955bc32b6383e778f91
skreider-ce <skreider@corevitas.com>
2024-02-15 15:56:24 -0500

    spacing

--- R/11_printCriticalCheck1.R
+++ R/11_printCriticalCheck1.R
@@ -3,11 +3,11 @@
 #' @param .criticalCheckId The id of the check to be printed (i.e. criticalCheck1 - criticalCheck8)
 #' @param .criticalCheckResults The listing output to be printed
 printCriticalCheck <- function(.criticalCheckId, .criticalCheckResults){
-  cat(paste0(.criticalCheckId, " \n"))
-  cat(paste0("Title: ", .criticalCheckResults$checkTitle, " \n"))
-  cat(paste0("Description: ", .criticalCheckResults$checkDescription, " \n"))
-  cat(paste0("Pass: ", .criticalCheckResults$pass, " \n"))
-  cat(paste0(" \n"))
+  cat(paste0(.criticalCheckId, "  \n"))
+  cat(paste0("Title: ", .criticalCheckResults$checkTitle, "  \n"))
+  cat(paste0("Description: ", .criticalCheckResults$checkDescription, "  \n"))
+  cat(paste0("Pass: ", .criticalCheckResults$pass, "  \n"))
+  cat(paste0("  \n"))
 
   # switch (.criticalCheckId,
   #   criticalCheck1 = {
--- R/scratch.R
+++ R/scratch.R
@@ -1,5 +1,5 @@
 # registrydqchecksreportdown::generateReport(
-#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/store/ad/2024-02-01/checks/2024-02-01_2024_02_15_13_26_52_checks.rds"
+#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/store/ad/2024-01-04/checks/2024-01-04_2024_02_15_15_49_00_checks.rds"
 #   ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/report/"
 #   ,.fileName = "outputReport"
 # )
