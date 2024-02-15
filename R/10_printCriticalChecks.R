5afb1f877e8a177c05e21c97fae5209caedccbb0
skreider-ce <skreider@corevitas.com>
2024-02-15 16:23:33 -0500

    table output

--- R/10_printCriticalChecks.R
+++ R/10_printCriticalChecks.R
@@ -3,8 +3,6 @@
 #' @param .criticalChecksToPrint An r dataframe with the critical checks to print
 printCriticalChecks <- function(.criticalChecksToPrint){
   for(criticalCheckId in names(.criticalChecksToPrint)){
-    cat(paste0("### ", criticalCheckId, " \n"))
-
     printCriticalCheck(criticalCheckId, .criticalChecksToPrint[[criticalCheckId]])
   }
 }
--- R/11_printCriticalCheck1.R
+++ R/11_printCriticalCheck1.R
@@ -2,37 +2,40 @@
 #'
 #' @param .criticalCheckId The id of the check to be printed (i.e. criticalCheck1 - criticalCheck8)
 #' @param .criticalCheckResults The listing output to be printed
+#'
+#' @importFrom reactable reactable
 printCriticalCheck <- function(.criticalCheckId, .criticalCheckResults){
-  cat(paste0(.criticalCheckId, "  \n"))
-  cat(paste0("Title: ", .criticalCheckResults$checkTitle, "  \n"))
+  cat(paste0("### ", .criticalCheckId, " - ", .criticalCheckResults$checkTitle , "  \n"))
   cat(paste0("Description: ", .criticalCheckResults$checkDescription, "  \n"))
   cat(paste0("Pass: ", .criticalCheckResults$pass, "  \n"))
   cat(paste0("  \n"))
 
-  # switch (.criticalCheckId,
-  #   criticalCheck1 = {
-  #
-  #   },
-  #   criticalCheck2 = {
-  #
-  #   },
-  #   criticalCheck3 = {
-  #
-  #   },
-  #   criticalCheck4 = {
-  #
-  #   },
-  #   criticalCheck5 = {
-  #
-  #   },
-  #   criticalCheck6 = {
-  #
-  #   },
-  #   criticalCheck7 = {
-  #
-  #   },
-  #   criticalCheck8 = {
-  #
-  #   }
-  # )
+  switch (.criticalCheckId,
+    "criticalCheck1" = {
+      print(NULL)
+    },
+    "criticalCheck2" = {
+      print(NULL)
+    },
+    "criticalCheck3" = {
+      print(NULL)
+    },
+    "criticalCheck4" = {
+      print(NULL)
+    },
+    "criticalCheck5" = {
+      print(NULL)
+    },
+    "criticalCheck6" = {
+      print(.criticalCheckResults$inOldAndNotInNew)
+      # reactable::reactable(.criticalCheckResults[[.criticalCheckId]]$inOldAndNotInNew)
+    },
+    "criticalCheck7" = {
+      print(NULL)
+    },
+    "criticalCheck8" = {
+      print(NULL)
+    }
+  )
 }
+
