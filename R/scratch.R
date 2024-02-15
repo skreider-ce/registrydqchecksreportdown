acdf606362ccb29707d3cae952caa66a77693cd6
skreider-ce <skreider@corevitas.com>
2024-02-15 10:32:51 -0500

    rproj

---
+++ R/scratch.R
@@ -0,0 +1,4 @@
+# registrydqchecksreportdown::generateReport(
+#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/store/ad/2024-02-01/checks/2024-02-01_2024_02_14_13_55_40_checks.rds"
+#   ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/report/"
+# )
