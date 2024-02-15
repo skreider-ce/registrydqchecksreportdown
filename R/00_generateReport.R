115218db0f81c1a7a592c31968e690b7422b00b1
skreider-ce <skreider@corevitas.com>
2024-02-15 12:29:58 -0500

    added filename

--- R/00_generateReport.R
+++ R/00_generateReport.R
@@ -2,15 +2,16 @@
 #'
 #' @param .inputDatasetUrl direct link to the .RDS file that has the check report output
 #' @param .reportOutputUrl link to the folder where the report will be stored
+#' @param .fileName name of the output file
 #'
 #' @export
 #'
 #' @importFrom glue glue
 #' @importFrom rmarkdown render
-generateReport <- function(.inputDatasetUrl, .reportOutputUrl){
+generateReport <- function(.inputDatasetUrl, .reportOutputUrl, .fileName){
   .checkDataset <- readRDS(glue::glue("{.inputDatasetUrl}"))
 
-  rmarkdown::render("./inst/rmarkdown/template.Rmd"
+  rmarkdown::render(glue::glue("./inst/rmarkdown/{.fileName}.Rmd")
                     ,output_dir = .reportOutputUrl
                     ,params = list(checkDataset = .checkDataset))
 }
