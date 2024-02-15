22d3c64a6db2960b78c13446477a74422e683f47
skreider-ce <skreider@corevitas.com>
2024-02-15 13:38:03 -0500

    Update scratch.R

--- R/scratch.R
+++ R/scratch.R
@@ -1,5 +1,5 @@
-registrydqchecksreportdown::generateReport(
-  .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/store/ad/2024-02-01/checks/2024-02-01_2024_02_15_13_26_52_checks.rds"
-  ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/report/"
-  ,.fileName = "outputReport"
-)
+# registrydqchecksreportdown::generateReport(
+#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/store/ad/2024-02-01/checks/2024-02-01_2024_02_15_13_26_52_checks.rds"
+#   ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/report/"
+#   ,.fileName = "outputReport"
+# )
