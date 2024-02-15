c1f3d5d66bb6dc69a3eb0cb942c5e5ee775aafd3
skreider-ce <skreider@corevitas.com>
2024-02-15 09:32:38 -0500

    add rmarkdown template

--- R/00_generateReport.R
+++ R/00_generateReport.R
@@ -1,13 +1,16 @@
 #' generateReport
 #'
-#' @param .inputDatasetUrl
-#' @param .reportOutputUrl
+#' @param .inputDatasetUrl direct link to the .RDS file that has the check report output
+#' @param .reportOutputUrl link to the folder where the report will be stored
 #'
 #' @export
 #'
 #' @importFrom glue glue
+#' @importFrom bookdown render_book
 generateReport <- function(.inputDatasetUrl, .reportOutputUrl){
   .checkDataset <- readRDS(glue::glue("{.inputDatasetUrl}"))
 
-  print(.checkDataset)
+  bookdown::render_book("./inst/rmarkdown/template.Rmd"
+                        ,output_format = "bookdown::html_document2"
+                        ,output_dir = .reportOutputUrl)
 }
