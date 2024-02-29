942a6ea964c7e1ad75fe290899a72000e7ee712d
skreider-ce <skreider@corevitas.com>
2024-02-29 14:30:37 -0500

    updating codebook nc output

--- R/scratch.R
+++ R/scratch.R
@@ -1,6 +1,9 @@
 # registrydqchecksreportdown::generateReport(
-#   .inputDatasetUrl = "C:/Users/ScottKreider/OneD - Corrona LLC/Corrona LLC/Biostat Data Files - DQ Reports/ad/2024/2024-02-01/2024_02_22_09_04_04/checks/2024-02-01_2024_02_22_09_04_04_checks.rds"
+#   .inputDatasetUrl = "C:/Users/ScottKreider/OneD - Corrona LLC/Corrona LLC/Biostat Data Files - DQ Reports/ad/2024/2024-02-01/2024_02_29_14_07_03/checks/2024-02-01_2024_02_29_14_07_03_checks.rds"
 #   ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/report/"
 #   ,.fileName = "outputReport"
 # )
+
+# .checkDataset$nonCriticalChecks[["exvisit"]]$codebookChecks
+# checkDataset$nonCriticalChecks[[datasetName]]$codebookChecks[[.noncriticalCheckId]]$values$n
 # ensuring up to date
