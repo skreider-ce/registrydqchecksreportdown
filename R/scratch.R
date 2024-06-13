fbcb563925bc277da402f09e2841a1610e7c73ba
skreider-ce <skreider@corevitas.com>
2024-06-13 10:02:24 -0400

    Logic to only print if values in list

--- R/scratch.R
+++ R/scratch.R
@@ -1,5 +1,5 @@
 # generateReport(
-#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/exampleOutput/ms_2024-05-05_2024-05-15_1343_checks.rds"
+#   .inputDatasetUrl = "C:/Users/ScottKreider/Documents/scrap/exampleOutput/ms_2024-06-05_2024-06-13_0947_checks.rds"
 #   ,.reportOutputUrl = "C:/Users/ScottKreider/Documents/scrap/exampleOutput/"
 #   ,.fileName = "outputReport"
 # )
